dc461643ebdcdd5494829c1885f9e9f38e34ef78
sandrooliveira1501 <sandrooliveira1501@gmail.com>
2016-08-03 18:26:18 -0300

    Logout implementado

--- js/controllers/main-app-controller.js
+++ js/controllers/main-app-controller.js
@@ -5,13 +5,20 @@
     // pegando dependência dos controladores do módulo e adicionando novo controlados ao módulo
     var controllers = angular.module('starter.controllers');
 
-    function MainAppController($scope, $http) {
+    function MainAppController($scope, $rootScope, $state) {
 
         
+        $scope.logout = function(){
+            $rootScope.user = undefined;
+            $rootScope.auth = undefined;
+            localStorage.user = undefined;
+            localStorage.token = undefined;
+            $state.go('app');
+        }
 
     }
 
-    MainAppController.$inject  = ["$scope","$http"];
+    MainAppController.$inject  = ["$scope","$rootScope","$state"];
 
     module.exports = controllers.controller("MainAppController", MainAppController);
 
