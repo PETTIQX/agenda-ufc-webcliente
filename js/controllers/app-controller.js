9bde14e0555283325420e8c29ff84b4be690fd4c
Jordy <jordyfgomes@gmail.com>
2016-08-09 09:10:01 -0300

    resolvendo conflito

--- js/controllers/app-controller.js
+++ js/controllers/app-controller.js
@@ -4,16 +4,10 @@
 
   var controllers = angular.module('starter.controllers');
 
-<<<<<<< HEAD
   function AppController($scope, $rootScope, AtividadeService) {
-=======
-  function AppController($scope,$http, xauth) {
-
->>>>>>> master
-
    	$scope.init	 = function(){
 
-   		$scope.atividades = [];      	
+   		$scope.atividades = [];
 
    		AtividadeService.buscaEventos().then(
 	   		function(response){
@@ -23,26 +17,12 @@
 	   		function(error){
 	   			console.log(error.data);
 	   		}
-   		)	
+   		)
 
    	};
-
-<<<<<<< HEAD
-
   }
 
   AppController.$inject = ["$scope","$rootScope", "AtividadeService"];
-=======
-    
-
-  }
-
-  controllers.value('xauth','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6Ij\
-  AzNTM1MDIxIiwiX2lkIjoiNTc4ZTM3MDVhOTYzNDZjMjU3NDQ5NWQwIiwiZWRpdG9yIjp0cnVlLCJ2aW5jdWxhY2FvIjowfQ.SEYK5_QGiCRKFCYiaJMtEmTZnYIXLNmkf0xQ4VfE33o');
-
-
-  AppController.$inject = ["$scope","$rootScope"];
->>>>>>> master
 
   module.exports = controllers.controller("AppController", ['$scope','$http','xauth',AppController]);
 })();
